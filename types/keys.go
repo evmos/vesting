4c2002b4102168bcadfb9f59744e90e884f600c4
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-08-17 14:13:54 +0300

    Add the vesting module without the tests for testing

---
+++ types/keys.go
@@ -0,0 +1,24 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package types
+
+const (
+	// prefixGovClawbackDisabledKey to be used in the KVStore to track vesting accounts that are not subject
+	// to clawback from governance.
+	prefixGovClawbackDisabledKey = iota + 1
+)
+
+// KeyPrefixGovClawbackDisabledKey is the slice of prefix bytes for storing the governance clawback enabled/disabled flag.
+var KeyPrefixGovClawbackDisabledKey = []byte{prefixGovClawbackDisabledKey}
+
+const (
+	// ModuleName defines the module's name.
+	ModuleName = "vesting"
+
+	// StoreKey to be used when creating the KVStore
+	StoreKey = ModuleName
+
+	// RouterKey defines the module's message routing key
+	RouterKey = ModuleName
+)
